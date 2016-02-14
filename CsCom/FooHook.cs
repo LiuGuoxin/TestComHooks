dc3d942b7943dd3026d798889c0ef650b12dc672
Jacob Buysse <Jacob.Buysse@gmail.com>
2016-02-13 23:52:20 -0600

    Refactoring FooHook to extract reusable ReleaseHooker.

--- CsCom/FooHook.cs
+++ CsCom/FooHook.cs
@@ -10,11 +10,12 @@ namespace CsCom
 	public class FooHook : CppCom.IFoo, IDisposable
 	{
 		private bool disposed;
+		private readonly ReleaseHooker releaseHooker;
 
 		public FooHook()
 		{
 			Console.WriteLine("C# FooHook(ctor)");
-			InstallHooks();
+			releaseHooker = new ReleaseHooker(this, typeof(CppCom.IFoo));
 		}
 
 		~FooHook()
@@ -35,51 +36,14 @@ namespace CsCom
 			if (disposed)
 				return;
 			disposed = true;
+			if (releaseManagedReferences)
+				releaseHooker.ReleaseMarshalledDelegate();
 			//TODO: other disposal taking into account releaseManagedReferences
 		}
 
 		public void Bar()
 		{
 			Console.WriteLine("C# FooHook.Bar()");
-		}
-
-		private delegate int ReleaseDelegate(IntPtr unknown);
-		private int ReleaseDetour(IntPtr unknown)
-		{
-			var result = release(unknown);
-			Console.WriteLine($"C# ReleaseDetour(new count={result})");
-			if (result == 0)
-				Dispose();
-			return result;
-		}
-
-		private ReleaseDelegate release;
-		private ReleaseDelegate releaseDetour;	//Must hang on to delegate to prevent GC
-
-		private void InstallHooks()
-		{
-			releaseDetour = ReleaseDetour;
-			var nativeReleaseDetour = Marshal.GetFunctionPointerForDelegate(releaseDetour);
-			InstallHook(Marshal.GetIUnknownForObject(this), nativeReleaseDetour);
-
-			InstallHook(Marshal.GetComInterfaceForObject(this, typeof(CppCom.IFoo)), nativeReleaseDetour);
-			InstallHook(Marshal.GetComInterfaceForObject(this, typeof(IDisposable)), nativeReleaseDetour);
-
-			//TODO: GetComInterfaceForObject does not work for typeof(object), so what about the _Object interface?
-			//Would need to do this for IDispatch (via GetIDispatchForObject) if it supported it
-			//Would need to do this for all supported interfaces in case that interface is the last reference
-			//TODO: What about other special interfaces automatically supported like:
-			//	ITypeInfo, IObjectSafety, ISupportErrorInfo, IMarshal, IReflect, IDispatchEx, IEnumVARIANT, etc.?
-		}
-
-		private void InstallHook(IntPtr comInterface, IntPtr nativeReleaseDetour)
-		{
-			var vtable = Marshal.ReadIntPtr(comInterface);
-			var nativeRelease = Marshal.ReadIntPtr(vtable, 2 * IntPtr.Size);
-			Marshal.WriteIntPtr(vtable, 2 * IntPtr.Size, nativeReleaseDetour);
-			if (release == null)
-				release = Marshal.GetDelegateForFunctionPointer<ReleaseDelegate>(nativeRelease);
-			release(comInterface);
 		}
 	}
 }
---
+++ CsCom/ReleaseHooker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CsCom
+{
+	public class ReleaseHooker
+	{
+		private delegate int ReleaseDelegate(IntPtr unknown);
+		private ReleaseDelegate originalRelease;
+		private ReleaseDelegate releaseDetour;
+		private readonly IDisposable comObject;
+
+		public ReleaseHooker(IDisposable comObject, params Type[] interfaces)
+		{
+			this.comObject = comObject;
+			releaseDetour = ReleaseDetour;
+
+			var releaseDetourPointer = Marshal.GetFunctionPointerForDelegate(releaseDetour);
+			InstallHook(Marshal.GetIUnknownForObject(comObject), releaseDetourPointer);
+			InstallHook(Marshal.GetComInterfaceForObject(comObject, typeof(IDisposable)), releaseDetourPointer);
+			foreach (var @interface in interfaces)
+				InstallHook(Marshal.GetComInterfaceForObject(comObject, @interface), releaseDetourPointer);
+
+			//TODO: GetComInterfaceForObject does not work for typeof(object), so what about the _Object interface?
+			//Would need to do this for IDispatch (via GetIDispatchForObject) if it supported it
+			//TODO: What about other special interfaces automatically supported like:
+			//	ITypeInfo, IObjectSafety, ISupportErrorInfo, IMarshal, IReflect, IDispatchEx, IEnumVARIANT, etc.?
+		}
+
+		private int ReleaseDetour(IntPtr unknown)
+		{
+			var referenceCount = originalRelease(unknown);
+			if (referenceCount == 0)
+				comObject.Dispose();
+			return referenceCount;
+		}
+
+		private void InstallHook(IntPtr comInterface, IntPtr releaseDetourPointer)
+		{
+			var vtable = Marshal.ReadIntPtr(comInterface);
+			var originalReleasePointer = Marshal.ReadIntPtr(vtable, 2 * IntPtr.Size);
+			Marshal.WriteIntPtr(vtable, 2 * IntPtr.Size, releaseDetourPointer);
+			if (originalRelease == null)
+				originalRelease = Marshal.GetDelegateForFunctionPointer<ReleaseDelegate>(originalReleasePointer);
+			originalRelease(comInterface);
+		}
+
+		public void ReleaseMarshalledDelegate()
+		{
+			releaseDetour = null;
+		}
+	}
+}
